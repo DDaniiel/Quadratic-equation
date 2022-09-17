9b9963d307cefcec2923c08ee3d7514ec6c13c05
DDaniiel <113246750+DDaniiel@users.noreply.github.com>
2022-09-17 15:58:31 +0200

    Update Quadratic.php

--- Quadratic.php
+++ Quadratic.php
@@ -8,8 +8,8 @@ if ($d > 0) {
     $D = sqrt($b*$b-4*$a*$c);
     $x1 = ((-$b - $D)/2*$a);  
     $x2 = ((-$b + $D)/2*$a);
-    echo ("  x1 = ".$x1);
-    echo ("  x2 = ".$x2);
+    echo "<span style = \" color: green;\">   x1 =</span>" .$x1;
+    echo "<span style = \" color: green;\">   x2 =</span>" .$x2;
 
 } 
 elseif ($d == 0) {
@@ -20,4 +20,3 @@ elseif ($d == 0) {
 elseif ($d < 0) {
     echo "<span style = \" color: red;\">   нет решений</span>";
 }
-
